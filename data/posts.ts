727c8825e18e773b85750d178d02cbf7d9a58067
Jéssica Rodrigues <jeehusagiim@gmail.com>
2024-07-25 08:47:24 -0300

    Add projects files

---
+++ data/posts.ts
@@ -0,0 +1,180 @@
+import { Post } from '@/types/posts'
+
+const posts: Post[] = [
+  {
+    id: '1',
+    title: 'The Rise of Chic Fashion Trends',
+    body: 'Chic fashion trends are revolutionizing the way we dress, bringing innovation and style to the forefront. Discover the latest in cute clothing, from adorable dresses to trendy accessories that are transforming wardrobes everywhere.',
+    author: 'John Doe',
+    date: '2024-05-01',
+    comments: [
+      { id: '1', text: 'Great introduction to new styles!', username: 'Jane' },
+      {
+        id: '2',
+        text: 'Looking forward to more posts on fashion trends.',
+        username: 'Alex',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Decorating with a Touch of Elegance',
+    body: 'Transform your living space with elegant and cute home decor items. From cozy throw pillows to charming wall art, find out how to create a beautiful and inviting environment with ease',
+    author: 'Emily Smith',
+    date: '2024-04-28',
+    comments: [
+      { id: '1', text: 'Fascinating read on home decor!!', username: 'Mark' },
+      {
+        id: '2',
+        text: 'I have some questions about styling tips.',
+        username: 'Sarah',
+      },
+    ],
+  },
+  {
+    id: '3',
+    title: 'Accessorize Your Outfit: The Key to a Stylish Look',
+    body: 'Accessories are the secret to elevating any outfit. Discover how cute and stylish accessories can transform your look and add that perfect finishing touch.',
+    author: 'David Johnson',
+    date: '2024-05-03',
+    comments: [
+      {
+        id: '1',
+        text: 'Accessories have immense potential!',
+        username: 'Lucy',
+      },
+      {
+        id: '2',
+        text: "I'm curious about the latest trends in accessories.",
+        username: 'Michael',
+      },
+    ],
+  },
+  {
+    id: '4',
+    title: 'Future Trends in Cute Home Decor',
+    body: 'Augmented Reality (AR) is even making waves in home decor! Learn how to use AR to envision cute and trendy decor in your home before making a purchase.',
+    author: 'Sophia Williams',
+    date: '2024-05-05',
+    comments: [
+      { id: '1', text: 'AR applications are amazing!', username: 'Grace' },
+      {
+        id: '2',
+        text: "Can't wait to see AR integrated into everyday shopping.",
+        username: 'Jack',
+      },
+    ],
+  },
+  {
+    id: '5',
+    title: 'Smart Homes: The Intersection of Technology and Cute Decor...',
+    body: "The Internet of Things (IoT) is not just about technology. it's also about creating a smart and stylish home. Discover how IoT can help you achieve a connected and cute living space.",
+    author: 'Andrew Brown',
+    date: '2024-05-08',
+    comments: [
+      {
+        id: '1',
+        text: 'IoT has huge potential for smart homes.',
+        username: 'Olivia',
+      },
+      {
+        id: '2',
+        text: 'Security concerns need to be addressed.',
+        username: 'William',
+      },
+    ],
+  },
+  {
+    id: '6',
+    title: '5G Technology: Revolutionizing Online Shopping',
+    body: '5G technology promises faster speeds and better connectivity, making online shopping for cute clothing and accessories more seamless and enjoyable than ever.',
+    author: 'Emma Davis',
+    date: '2024-05-10',
+    comments: [
+      {
+        id: '1',
+        text: 'Excited for the possibilities with 5G.',
+        username: 'Sophie',
+      },
+      {
+        id: '2',
+        text: 'Hope it improves rural connectivity too.',
+        username: 'Ethan',
+      },
+    ],
+  },
+  {
+    id: '7',
+    title: 'Protecting Your Cute Purchases Online: Cybersecurity Tips',
+    body: 'As online shopping grows, so do the concerns about cybersecurity. Learn how to protect your data and ensure a safe shopping experience when buying your favorite cute items.',
+    author: 'Michael Johnson',
+    date: '2024-05-12',
+    comments: [
+      {
+        id: '1',
+        text: 'Cybersecurity is crucial for protecting data.',
+        username: 'Ava',
+      },
+      { id: '2', text: 'Continuous monitoring is key.', username: 'Noah' },
+    ],
+  },
+  {
+    id: '8',
+    title: 'Inspired by Nature: Adorable Stationery Trends',
+    body: 'Artificial Neural Networks (ANNs) might inspire tech innovations, but nature inspires our cute stationery designs! Discover the latest trends in adorable notebooks, planners, and more.',
+    author: 'Isabella White',
+    date: '2024-05-15',
+    comments: [
+      {
+        id: '1',
+        text: 'Cute stationery has applications in various fields.',
+        username: 'Liam',
+      },
+      {
+        id: '2',
+        text: 'Finding the perfect design requires a lot of creativity.',
+        username: 'Mia',
+      },
+    ],
+  },
+  {
+    id: '9',
+    title: 'Cloud Computing: Enhancing Your E-Commerce Experience',
+    body: 'Cloud computing is revolutionizing how we shop online. Find out how this technology is enabling scalable solutions for better access to the cutest fashion, accessories, and decor items.',
+    author: 'James Taylor',
+    date: '2024-05-18',
+    comments: [
+      {
+        id: '1',
+        text: 'Cloud computing has transformed IT.',
+        username: 'Harper',
+      },
+      {
+        id: '2',
+        text: 'Concerned about data privacy in the cloud.',
+        username: 'Logan',
+      },
+    ],
+  },
+  {
+    id: '10',
+    title: 'Machine Learning: Discover Your Perfect Outfit  ',
+    body: 'Machine Learning (ML) algorithms are now helping shoppers find the perfect cute outfit. Learn how ML is reshaping the fashion industry and making shopping more personalized.',
+    author: 'Ava Johnson',
+    date: '2024-05-20',
+    comments: [
+      {
+        id: '1',
+        text: 'ML is reshaping industries, including fashion.',
+        username: 'Emma',
+      },
+      {
+        id: '2',
+        text: "I'm interested in how ML can suggest outfits.",
+        username: 'Daniel',
+      },
+    ],
+  },
+]
+
+export default posts
